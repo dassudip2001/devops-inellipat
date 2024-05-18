bb60fc4f5a842430ccbea89b275a1b180f3d3467
Sudip Das <sudip5428@gmail.com>
2024-05-18 10:44:12 +0530

    Code commit for mongoose setup '

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require("express");
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
-
+const colors = require("colors");
+const database = require("./config/db");
 const port = process.env.APP_PORT || 4000;
 const app = express();
+
+database();
 
 app.use(
   "/graphql",
